89c5968df54b0187e27f3b2b9b4ec6b2205bc595
Gopal chandrashekhar Gawas <146362802+Gopal562004@users.noreply.github.com>
2025-08-15 01:14:04 +0530

    Update next.config.ts

--- nextjs/next.config.ts
+++ nextjs/next.config.ts
@@ -1,26 +1,56 @@
-// next.config.js
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV !== 'production',
-  cacheOnFrontEndNav: true,
-  fallbacks: {
-    document: '/offline.html'
-  }
-});
+// // next.config.js
+// const withPWA = require('next-pwa')({
+//   dest: 'public',
+//   register: true,
+//   skipWaiting: true,
+//   disable: process.env.NODE_ENV !== 'production',
+//   cacheOnFrontEndNav: true,
+//   fallbacks: {
+//     document: '/offline.html'
+//   }
+// });
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   images: {
+//     domains: ['www.themealdb.com']
+//   },
+//   eslint: {
+//     ignoreDuringBuilds: true
+//   },
+//   typescript: {
+//     ignoreBuildErrors: true
+//   }
+// };
+
+// module.exports = withPWA(nextConfig);
+// next.config.ts
+import withPWA from 'next-pwa';
+import { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  reactStrictMode: true,
   images: {
-    domains: ['www.themealdb.com']
+    domains: ['www.themealdb.com'],
   },
   eslint: {
-    ignoreDuringBuilds: true
+    ignoreDuringBuilds: true,
   },
   typescript: {
-    ignoreBuildErrors: true
-  }
+    ignoreBuildErrors: true,
+  },
 };
 
-module.exports = withPWA(nextConfig);
+export default withPWA({
+  ...nextConfig,
+  pwa: {
+    dest: 'public',
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV !== 'production',
+    cacheOnFrontEndNav: true,
+    fallbacks: {
+      document: '/offline.html',
+    },
+  },
+});
